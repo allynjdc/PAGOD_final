007ad4a2dec59cf46c38704cd5cf53ce9d0c485b
AJ Calcaben <calcabenallynjoy@gmail.com>
2018-02-07 13:46:56 +0800

    pulling

---
+++ resources/views/studyplan.blade.php
@@ -0,0 +1,500 @@
+@extends("")
+
+@section('content')
+	
+	<!-- MIDDLE PART -->
+	<div class="container index_container">
+
+		<!-- Column md 8 -->
+		<div class="col-md-8 col-sm-12">
+			<div class="panel panel-default">
+				<div class="panel-heading">
+					<h4><strong>FIRST YEAR</strong> <small class="pull-right">A.Y. 2014-2015</small></h4>
+				</div>
+				<div class="table-responsive">
+					<table class="table table-hover">
+						<thead>
+							<tr>
+								<th>First Semester</th>
+								<th>Units</th>
+								<th>Completion</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td>Math 17</td>
+								<td>5.0</td>
+								<td><span class="glyphicon glyphicon-ok"></span></td>
+							</tr>
+							<tr>
+								<td>CMSC 11</td>
+								<td>3.0</td>
+								<td><span class="glyphicon glyphicon-ok"></span></td>
+							</tr>
+							<tr>
+								<td>GE (AH)</td>
+								<td>3.0</td>
+								<td><span class="glyphicon glyphicon-ok"></span></td>
+							</tr>
+							<tr>
+								<td>GE (SSP)</td>
+								<td>3.0</td>
+								<td><span class="glyphicon glyphicon-ok"></span></td>
+							</tr>
+							<tr>
+								<td>PE 1</td>
+								<td>(2.0)</td>
+								<td><span class="glyphicon glyphicon-ok"></span></td>
+							</tr>
+							<tr>
+								<td>GE (MST)</td>
+								<td>3.0</td>
+								<td><span class="glyphicon glyphicon-ok"></span></td>
+							</tr>
+							<tr>
+								<td>NSTP</td>
+								<td>(3.0)</td>
+								<td><span class="glyphicon glyphicon-ok"></span></td>
+							</tr>
+						</tbody>
+						<tfoot>
+							<tr class="bg-success">
+								<td><strong>TOTAL</strong></td>
+								<td>17 units</td>
+								<td><span class="glyphicon glyphicon-ok"></span></td>
+							</tr>
+						</tfoot>
+					</table>
+				</div>
+				<div class="table-responsive">
+					<table class="table table-hover">
+						<thead>
+							<tr>
+								<th>Second Semester</th>
+								<th>Units</th>
+								<th>Completion</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td>Math 100</td>
+								<td>4.0</td>
+								<td><span class="glyphicon glyphicon-ok"></span></td>
+							</tr>
+							<tr>
+								<td>CMSC 21</td>
+								<td>3.0</td>
+								<td><span class="glyphicon glyphicon-ok"></span></td>
+							</tr>
+							<tr>
+								<td>CMSC 56</td>
+								<td>3.0</td>
+								<td><span class="glyphicon glyphicon-ok"></span></td>
+							</tr>
+							<tr>
+								<td>NSTP</td>
+								<td>(3.0)</td>
+								<td><span class="glyphicon glyphicon-ok"></span></td>
+							</tr>
+							<tr>
+								<td>PE</td>
+								<td>(2.0)</td>
+								<td><span class="glyphicon glyphicon-ok"></span></td>
+							</tr>
+							<tr>
+								<td>GE (SSP)</td>
+								<td>3.0</td>
+								<td><span class="glyphicon glyphicon-ok"></span></td>
+							</tr>
+							<tr>
+								<td>GE (AH)</td>
+								<td>3.0</td>
+								<td><span class="glyphicon glyphicon-ok"></span></td>
+							</tr>
+							<tr>
+								<td>GE (MST)</td>
+								<td>3.0</td>
+								<td><span class="glyphicon glyphicon-ok"></span></td>
+							</tr>
+						</tbody>
+						<tfoot>
+							<tr class="bg-success">
+								<td><strong>TOTAL</strong></td>
+								<td>19 units</td>
+								<td><span class="glyphicon glyphicon-ok"></span></td>
+							</tr>
+						</tfoot>
+					</table>
+				</div>
+			</div>
+			<div class="panel panel-default">
+				<div class="panel-heading">
+					<h4><strong>SECOND YEAR</strong> <small class="pull-right">A.Y. 2015-2016</small></h4>
+				</div>
+				<div class="table-responsive">
+					<table class="table table-hover">
+						<thead>
+							<tr>
+								<th>First Semester</th>
+								<th>Units</th>
+								<th>Completion</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td>GE (AH)</td>
+								<td>3.0</td>
+								<td><span class="glyphicon glyphicon-ok"></span></td>
+							</tr>
+							<tr>
+								<td>GE (SSP)</td>
+								<td>3.0</td>
+								<td><span class="glyphicon glyphicon-ok"></span></td>
+							</tr>
+							<tr>
+								<td>GE (MST)</td>
+								<td>3.0</td>
+								<td><span class="glyphicon glyphicon-ok"></span></td>
+							</tr>
+							<tr>
+								<td>CMSC 22</td>
+								<td>3.0</td>
+								<td><span class="glyphicon glyphicon-ok"></span></td>
+							</tr>
+							<tr>
+								<td>CMSC 57</td>
+								<td>3.0</td>
+								<td><span class="glyphicon glyphicon-ok"></span></td>
+							</tr>
+							<tr>
+								<td>PHYSICS 51</td>
+								<td>3.0</td>
+								<td><span class="glyphicon glyphicon-ok"></span></td>
+							</tr>
+							<tr>
+								<td>PHYSICS 51.1</td>
+								<td>1.0</td>
+								<td><span class="glyphicon glyphicon-ok"></span></td>
+							</tr>
+							<tr>
+								<td>PE</td>
+								<td>(2.0)</td>
+								<td><span class="glyphicon glyphicon-ok"></span></td>
+							</tr>
+						</tbody>
+						<tfoot>
+							<tr class="bg-success">
+								<td><strong>TOTAL</strong></td>
+								<td>19 units</td>
+								<td><span class="glyphicon glyphicon-ok"></span></td>
+							</tr>
+						</tfoot>
+					</table>
+				</div>
+				<div class="table-responsive">
+					<table class="table table-hover">
+						<thead>
+							<tr>
+								<th>Second Semester</th>
+								<th>Units</th>
+								<th>Completion</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td>GE (SSP)</td>
+								<td>3.0</td>
+								<td><span class="glyphicon glyphicon-ok"></span></td>
+							</tr>
+							<tr>
+								<td>GE (AH)</td>
+								<td>3.0</td>
+								<td><span class="glyphicon glyphicon-ok"></span></td>
+							</tr>
+							<tr>
+								<td>GE (MST)</td>
+								<td>3.0</td>
+								<td><span class="glyphicon glyphicon-ok"></span></td>
+							</tr>
+							<tr>
+								<td>CMSC 123</td>
+								<td>3.0</td>
+								<td><span class="glyphicon glyphicon-ok"></span></td>
+							</tr>
+							<tr>
+								<td>PHYSICS 52</td>
+								<td>3.0</td>
+								<td><span class="glyphicon glyphicon-ok"></span></td>
+							</tr>
+							<tr>
+								<td>PHYSICS 52.1</td>
+								<td>3.0</td>
+								<td><span class="glyphicon glyphicon-ok"></span></td>
+							</tr>
+							<tr>
+								<td>CMSC 130</td>
+								<td>3.0</td>
+								<td><span class="glyphicon glyphicon-ok"></span></td>
+							</tr>
+							<tr>
+								<td>PE</td>
+								<td>(2.0)</td>
+								<td><span class="glyphicon glyphicon-ok"></span></td>
+							</tr>
+						</tbody>
+						<tfoot>
+							<tr class="bg-success">
+								<td><strong>TOTAL</strong></td>
+								<td>19 units</td>
+								<td><span class="glyphicon glyphicon-ok"></span></td>
+							</tr>
+						</tfoot>
+					</table>
+				</div>
+			</div>
+			<div class="panel panel-default">
+				<div class="panel-heading">
+					<h4><strong>THIRD YEAR</strong> <small class="pull-right">A.Y. 2016-2017</small></h4>
+				</div>
+				<div class="table-responsive">
+					<table class="table table-hover">
+						<thead>
+							<tr>
+								<th>First Semester</th>
+								<th>Units</th>
+								<th>Completion</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td>GE (AH)</td>
+								<td>3.0</td>
+								<td><span class="glyphicon glyphicon-ok"></span></td>
+							</tr>
+							<tr>
+								<td>CMSC 127</td>
+								<td>3.0</td>
+								<td><span class="glyphicon glyphicon-ok"></span></td>
+							</tr>
+							<tr>
+								<td>CMSC 128</td>
+								<td>3.0</td>
+								<td><span class="glyphicon glyphicon-ok"></span></td>
+							</tr>
+							<tr>
+								<td>CMSC 131</td>
+								<td>3.0</td>
+								<td><span class="glyphicon glyphicon-ok"></span></td>
+							</tr>
+							<tr>
+								<td>STAT 105</td>
+								<td>3.0</td>
+								<td><span class="glyphicon glyphicon-ok"></span></td>
+							</tr>
+							<tr>
+								<td>CMSC 141</td>
+								<td>3.0</td>
+								<td><span class="glyphicon glyphicon-ok"></span></td>
+							</tr>
+						</tbody>
+						<tfoot>
+							<tr class="bg-success">
+								<td><strong>TOTAL</strong></td>
+								<td>18 units</td>
+								<td><span class="glyphicon glyphicon-ok"></span></td>
+							</tr>
+						</tfoot>
+					</table>
+				</div>
+				<div class="table-responsive">
+					<table class="table table-hover">
+						<thead>
+							<tr>
+								<th>Second Semester</th>
+								<th>Units</th>
+								<th>Completion</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td>ENG 10</td>
+								<td>3.0</td>
+								<td><span class="glyphicon glyphicon-ok"></span></td>
+							</tr>
+							<tr>
+								<td>CMSC 124</td>
+								<td>3.0</td>
+								<td><span class="glyphicon glyphicon-ok"></span></td>
+							</tr>
+							<tr>
+								<td>CMSC 125</td>
+								<td>3.0</td>
+								<td><span class="glyphicon glyphicon-ok"></span></td>
+							</tr>
+							<tr>
+								<td>CMSC 129</td>
+								<td>3.0</td>
+								<td><span class="glyphicon glyphicon-ok"></span></td>
+							</tr>
+							<tr>
+								<td>CMSC 132</td>
+								<td>3.0</td>
+								<td><span class="glyphicon glyphicon-ok"></span></td>
+							</tr>
+							<tr>
+								<td>CMSC 126</td>
+								<td>3.0</td>
+								<td><span class="glyphicon glyphicon-ok"></span></td>
+							</tr>
+						</tbody>
+						<tfoot>
+							<tr class="bg-success">
+								<td><strong>TOTAL</strong></td>
+								<td>18 units</td>
+								<td><span class="glyphicon glyphicon-ok"></span></td>
+							</tr>
+						</tfoot>
+					</table>
+				</div>
+				<div class="table-responsive">
+					<table class="table table-hover">
+						<thead>
+							<tr>
+								<th>Midyear</th>
+								<th>Units</th>
+								<th>Completion</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td>CMSC 195</td>
+								<td>3.0</td>
+								<td><span class="glyphicon glyphicon-ok"></span></td>
+							</tr>
+						</tbody>
+						<tfoot>
+							<tr class="bg-success">
+								<td><strong>TOTAL</strong></td>
+								<td>3 units</td>
+								<td><span class="glyphicon glyphicon-ok"></span></td>
+							</tr>
+						</tfoot>
+					</table>
+				</div>
+			</div>
+			<div class="panel panel-default">
+				<div class="panel-heading">
+					<h4><strong>FOURTH YEAR</strong> <small class="pull-right">A.Y. 2017-2018</small></h4>
+				</div>
+				<div class="table-responsive">
+					<table class="table table-hover">
+						<thead>
+							<tr>
+								<th>First Semester</th>
+								<th>Units</th>
+								<th>Completion</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td>GE (SSP)</td>
+								<td>3.0</td>
+								<td><span class="glyphicon glyphicon-option-horizontal"></span></td>
+							</tr>
+							<tr>
+								<td>CMSC 142</td>
+								<td>3.0</td>
+								<td><span class="glyphicon glyphicon-option-horizontal"></span></td>
+							</tr>
+							<tr>
+								<td>CMSC 137</td>
+								<td>3.0</td>
+								<td><span class="glyphicon glyphicon-option-horizontal"></span></td>
+							</tr>
+							<tr>
+								<td>CMSC 198.1</td>
+								<td>2.0</td>
+								<td><span class="glyphicon glyphicon-option-horizontal"></span></td>
+							</tr>
+							<tr>
+								<td>CMSC 192</td>
+								<td>1.0</td>
+								<td><span class="glyphicon glyphicon-option-horizontal"></span></td>
+							</tr>
+							<tr>
+								<td>ELECTIVE</td>
+								<td>3.0</td>
+								<td><span class="glyphicon glyphicon-option-horizontal"></span></td>
+							</tr>
+							<tr>
+								<td>ELECTIVE</td>
+								<td>3.0</td>
+								<td><span class="glyphicon glyphicon-option-horizontal"></span></td>
+							</tr>
+						</tbody>
+						<tfoot>
+							<tr class="bg-primary">
+								<td><strong>TOTAL</strong></td>
+								<td>18 units</td>
+								<td><span class="glyphicon glyphicon-option-horizontal"></span></td>
+							</tr>
+						</tfoot>
+					</table>
+				</div>
+				<div class="table-responsive">
+					<table class="table table-hover">
+						<thead>
+							<tr>
+								<th>Second Semester</th>
+								<th>Units</th>
+								<th>Completion</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td>PI 100</td>
+								<td>3.0</td>
+								<td><span class="glyphicon glyphicon-remove"></span></td>
+							</tr>
+							<tr>
+								<td>CMSC 198.2</td>
+								<td>3.0</td>
+								<td><span class="glyphicon glyphicon-remove"></span></td>
+							</tr>
+							<tr>
+								<td>ELECTIVE</td>
+								<td>3.0</td>
+								<td><span class="glyphicon glyphicon-remove"></span></td>
+							</tr>
+							<tr>
+								<td>ELECTIVE</td>
+								<td>3.0</td>
+								<td><span class="glyphicon glyphicon-remove"></span></td>
+							</tr>
+							<tr>
+								<td>ELECTIVE</td>
+								<td>3.0</td>
+								<td><span class="glyphicon glyphicon-remove"></span></td>
+							</tr>
+							<tr>
+								<td>ELECTIVE</td>
+								<td>3.0</td>
+								<td><span class="glyphicon glyphicon-remove"></span></td>
+							</tr>
+						</tbody>
+						<tfoot>
+							<tr class="bg-danger">
+								<td><strong>TOTAL</strong></td>
+								<td>17 units</td>
+								<td><span class="glyphicon glyphicon-remove"></span></td>
+							</tr>
+						</tfoot>
+					</table>
+				</div>
+			</div>
+		</div>
+		<!-- END Column md 8 -->
+	</div>
+	<!-- END MIDDLE PART -->
+	
+@endsection
