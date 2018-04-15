e29b5811308644a4e031fcc7efeb216c436e823f
AJ Calcaben <calcabenallynjoy@gmail.com>
2018-04-15 18:25:41 +0800

    updating /addpreferences

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Illuminate\Http\Request; 
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Input;
